cee325cd55aae126984b9f7f2b3cab65e36f936d
Wilson Cheah <wilsoncheah5@gmail.com>
2021-05-18 14:11:58 -0700

    created a Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div id="navbar">
+      <div id="logo">Logo</div>
+      <div id="account">Login/Logout/Register</div>
+    </div>
+  );
+};
